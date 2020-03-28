ae0bc337c4b9ab6c513497448acfe6f100ab8b86
Cameron Rhinehardt <rhinehardtc@gmail.com>
2020-03-28 21:56:55 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,8 +20,8 @@ def bonus
    }
   }
 
- epic_tragedy[:montague][:hero][:status] = "dead"
- epic_tragedy[:montague][:heroine][:status] = "dead"
+ bonus[:montague][:hero][:status] = "dead"
+ bonus[:montague][:heroine][:status] = "dead"
 
   
 
